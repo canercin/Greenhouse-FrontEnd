2f260350e270a0dbb2687d5ca2435263f3c4b508
canercin <canercincin34@gmail.com>
2024-06-11 16:22:33 +0300

    added get all data page

---
+++ src/app/alldatas/alldatas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alldatas',
+  standalone: true,
+  imports: [],
+  templateUrl: './alldatas.component.html',
+  styleUrl: './alldatas.component.css'
+})
+export class AlldatasComponent {
+
+}
