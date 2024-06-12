6468d367ada0659bba33035cbb2dd01b82e5e994
canercin <canercincin34@gmail.com>
2024-06-12 14:39:45 +0300

    some fixes on all datas component

--- src/app/alldatas/alldatas.component.ts
+++ src/app/alldatas/alldatas.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {SidebarComponent} from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-alldatas',
   standalone: true,
-  imports: [],
+    imports: [
+        SidebarComponent
+    ],
   templateUrl: './alldatas.component.html',
   styleUrl: './alldatas.component.css'
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {AllusersComponent} from "./allusers/allusers.component";
+import {AlldatasComponent} from "./alldatas/alldatas.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent, AllusersComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, AllusersComponent, AlldatasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
