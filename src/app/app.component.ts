5d76d86b3868ea38299d3732ae98dd0d146d1c4e
canercin <canercincin34@gmail.com>
2024-06-12 14:52:17 +0300

    git issue fixed again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-=======
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {AllusersComponent} from "./allusers/allusers.component";
->>>>>>> 04ccb7d (added sidebar and all users page)
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet],
-=======
   imports: [RouterOutlet, LoginComponent, RegisterComponent, AllusersComponent],
->>>>>>> 04ccb7d (added sidebar and all users page)
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
