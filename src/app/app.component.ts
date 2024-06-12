98078b2b5aae11a002c9ef152c3a86fb3e7994a0
canercin <canercincin34@gmail.com>
2024-06-12 16:48:17 +0300

    main page created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {AllusersComponent} from "./allusers/allusers.component";
+import {MainComponent} from "./main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent, AllusersComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, AllusersComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
