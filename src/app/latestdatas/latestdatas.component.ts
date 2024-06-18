0693d744b1ee35e9545a5b0580f264d4819a5715
canercin <canercincin34@gmail.com>
2024-06-18 13:09:43 +0300

    last ten data table created and added main page

---
+++ src/app/latestdatas/latestdatas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latestdatas',
+  standalone: true,
+  imports: [],
+  templateUrl: './latestdatas.component.html',
+  styleUrl: './latestdatas.component.css'
+})
+export class LatestdatasComponent {
+
+}
