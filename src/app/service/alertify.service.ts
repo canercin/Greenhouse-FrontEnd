200d94f4ea5d8a333750e232f4c66448836e47d1
canercin <canercincin34@gmail.com>
2024-06-19 19:50:00 +0300

    alertifyjs added and served with service

---
+++ src/app/service/alertify.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlertifyService {
+  private alertify: any;
+
+  constructor() {
+    this.alertify = (window as any).alertify;
+  }
+
+  success(message: string) {
+    this.alertify.success(message);
+  }
+}
